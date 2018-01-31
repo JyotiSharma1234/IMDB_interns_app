cb6ccd2c3fa75363cfb95b1e044db2c745de74f7
JyotiSharma1234 <jyoti@joshsoftware.com>
2018-01-31 13:22:47 +0530

    association done

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,2 +1,4 @@
 class Actor < ApplicationRecord
+  #has_and_belongs_to_many :movies
+  has_and_belongs_to_many :casts, :join_table => "casts", :class_name => "Movie"
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,4 @@
 class Movie < ApplicationRecord
+  #has_and_belongs_to_many :actors
+  has_and_belongs_to_many :casts, :join_table => "casts", :class_name => "Actor"
 end
