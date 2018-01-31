148f323024cb4ddedca6b1cfa205c3b3d032a3d7
Viraj Chavan <virajc@live.com>
2018-01-31 15:50:15 +0530

    some validation

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -15,9 +15,10 @@ class Actor < ApplicationRecord
   has_and_belongs_to_many :casts, :join_table => "casts", :class_name => "Movie"
   validates :name, presence: true, format: { with: /\A[a-zA-Z ]+\z/, message: 'Invalid' }
   validates :date_of_birth, presence: true
-  validates :description, presence: true, length: { in: 5..100}
+  validates :description, presence: true, length: { in: 5..100 }
+  validate :validate_date_of_birth
 
-  def is_valid_date?
-    date_of_birth < Time.now.strftime("%d-%m-%Y")
+  def validate_date_of_birth
+    errors.add("data_of_birth", "(Dates from future not allowed!)") unless date_of_birth < Time.now
   end
 end
