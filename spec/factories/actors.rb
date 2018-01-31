873ccf4da082a2426ea89d8c886aa5dba07c5a26
JyotiSharma1234 <jyoti@joshsoftware.com>
2018-01-31 15:17:14 +0530

    factory girl and rspec

---
+++ spec/factories/actors.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :user do
+  	name 'Jyoti'
+  	date_of_birth '28-09-1996'
+  	description 'Sample actor'
+  end
+ end
---
+++ spec/factories/movies.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :movie do
+  	name 'ABC'
+  	description 'sample movie description'
+  	rating '4'
+  	release_date '28-09-1996'
+  end
+ end
