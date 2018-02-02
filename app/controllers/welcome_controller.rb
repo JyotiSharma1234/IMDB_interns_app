3c5799a98350d9367665492a395b7778bdb2c40f
Viraj Chavan <virajc@live.com>
2018-02-02 11:08:51 +0530

    combined search done

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    @movie_results = Movie.search(params[:search_term])
+    @actor_results = Actor.search(params[:search_term])
   end
 end
